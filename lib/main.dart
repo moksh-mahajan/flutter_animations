d927f5bb597b061da2064fcf66c56c898df4be60
Moksh Mahajan <moksh@Mokshs-MacBook-Air.local>
2023-02-12 14:43:59 +0530

    rotate and flip chained animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations/rotate_and_flip_chained_animation_page.dart';
 import 'package:flutter_animations/rotating_container_page.dart';
 
 void main() => runApp(const App());
@@ -9,7 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: RotatingContainerPage(),
+      home: RotateAndFlipChainedAnimationPage(),
     );
   }
 }
---
+++ lib/rotate_and_flip_chained_animation_page.dart
@@ -0,0 +1,217 @@
+import 'dart:math' show pi;
+
+import 'package:flutter/material.dart';
+
+class RotateAndFlipChainedAnimationPage extends StatelessWidget {
+  const RotateAndFlipChainedAnimationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green.shade300,
+      body: const SafeArea(
+        child: Center(
+          child: _RotatingCircle(),
+        ),
+      ),
+    );
+  }
+}
+
+class _RotatingCircle extends StatefulWidget {
+  const _RotatingCircle({super.key});
+
+  @override
+  State<_RotatingCircle> createState() => _RotatingCircleState();
+}
+
+class _RotatingCircleState extends State<_RotatingCircle>
+    with TickerProviderStateMixin {
+  late AnimationController _counterClockwiseRotationController;
+  late Animation<double> _counterClockwiseRotationAnimation;
+
+  late AnimationController _flipController;
+  late Animation _flipAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _counterClockwiseRotationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    _counterClockwiseRotationAnimation =
+        Tween<double>(begin: 0, end: -pi / 2).animate(
+      CurvedAnimation(
+        parent: _counterClockwiseRotationController,
+        curve: Curves.bounceOut,
+      ),
+    );
+
+    _flipController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    _flipAnimation = Tween<double>(
+      begin: 0,
+      end: pi,
+    ).animate(
+      CurvedAnimation(
+        parent: _flipController,
+        curve: Curves.bounceOut,
+      ),
+    );
+
+    _counterClockwiseRotationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _flipAnimation = Tween<double>(
+          begin: _flipAnimation.value,
+          end: _flipAnimation.value + pi,
+        ).animate(
+          CurvedAnimation(
+            parent: _flipController,
+            curve: Curves.bounceOut,
+          ),
+        );
+        _flipController
+          ..reset()
+          ..forward();
+      }
+    });
+
+    _flipController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _counterClockwiseRotationAnimation = Tween<double>(
+          begin: _counterClockwiseRotationAnimation.value,
+          end: _counterClockwiseRotationAnimation.value - pi / 2,
+        ).animate(
+          CurvedAnimation(
+            parent: _counterClockwiseRotationController,
+            curve: Curves.bounceOut,
+          ),
+        );
+        _counterClockwiseRotationController
+          ..reset()
+          ..forward();
+      }
+    });
+
+    _counterClockwiseRotationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _counterClockwiseRotationController.dispose();
+    _flipController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _counterClockwiseRotationController,
+      builder: (context, child) {
+        return Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.identity()
+            ..rotateZ(_counterClockwiseRotationAnimation.value),
+          child: child,
+        );
+      },
+      child: AnimatedBuilder(
+        animation: _flipController,
+        builder: (context, child) {
+          return Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity()..rotateY(_flipAnimation.value),
+            child: child,
+          );
+        },
+        child: const TwoColorCircle(),
+      ),
+    );
+  }
+}
+
+class TwoColorCircle extends StatelessWidget {
+  const TwoColorCircle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ClipPath(
+          clipper: const HalfCircleClipper(side: CircleSide.left),
+          child: Container(
+            color: Colors.red,
+            height: 160,
+            width: 160,
+          ),
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        ClipPath(
+          clipper: const HalfCircleClipper(side: CircleSide.right),
+          child: Container(
+            color: Colors.white,
+            height: 160,
+            width: 160,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class HalfCircleClipper extends CustomClipper<Path> {
+  final CircleSide side;
+
+  const HalfCircleClipper({required this.side});
+
+  @override
+  Path getClip(Size size) => side.toPath(size);
+
+  @override
+  bool shouldReclip(covariant CustomClipper oldClipper) => true;
+}
+
+enum CircleSide {
+  left,
+  right,
+}
+
+extension ToPath on CircleSide {
+  Path toPath(Size size) {
+    final path = Path();
+    late Offset offset;
+    late bool clockwise;
+
+    switch (this) {
+      case CircleSide.left:
+        path.moveTo(size.width, 0);
+        offset = Offset(size.width, size.height);
+        clockwise = false;
+        break;
+      case CircleSide.right:
+        offset = Offset(0, size.height);
+        clockwise = true;
+        break;
+    }
+
+    path.arcToPoint(
+      offset,
+      radius: Radius.elliptical(
+        size.width / 2,
+        size.height / 2,
+      ),
+      clockwise: clockwise,
+    );
+    path.close();
+    return path;
+  }
+}
